cdee0b5970dd67b9ce17ea400521a40d17bc53fb
marktregub <mark.tregub@logz.io>
2018-12-30 22:28:05 +0200

    merge with the main functionality

--- questions_reader.py
+++ questions_reader.py
@@ -1,6 +1,8 @@
 import json
 from pprint import pprint
 import getpass
+import sys
+import random
 
 #pprint(data['questions'])
 
@@ -11,6 +13,18 @@ class Question:
         self.question = rawQ['question']
         self.answers = rawQ['answers']
         self.correct = rawQ['correct']
+
+    def ask(self):
+        print self.tostr()
+
+        response = int(sys.stdin.readline().strip())  # answers are integers
+        if response == self.correct:
+            print "CORRECT"
+            return 10
+        else:
+            print "WRONG"
+            return 0
+
 
     def tostr(self):
         res = self.question + "\n"
@@ -28,13 +42,14 @@ class Questions:
         for q in data['questions']:
             self.questions.append(Question(q))
 
+        random.shuffle(self.questions)
+
     def tostr(self, title):
         res = title + "\n"
         for i in range(len(self.questions)):
             res += str(i+1) + ". " + self.questions[i].tostr() + "\n"
         return res
 
-# print('User: ', getpass.getuser())
 # data = Questions('quiz.json')
 # print(len(data.questions))
 # print(data.tostr("QUIZZZZZZ"))
